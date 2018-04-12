2dce872d648b662b653f743d32a4fc4e3513672f
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2018-04-12 09:23:15 +0000

    Print output

--- scripts/alternatives_parameter_sweep.sh
+++ scripts/alternatives_parameter_sweep.sh
@@ -11,6 +11,7 @@ for overlap in "1.25" "1.33" "1.50" "1.75"; do
             for different in "0.5" "0.75" "0.85"; do
                 for optimal in "0.1" "0.2" "0.3"; do
                     OUTPUT="${overlap}_${unpack}_${longer}_${different}_${optimal}_mld.csv"
+                    echo "###### $OUTPUT ######"
                     OSRM_OVERLAP=$overlap OSRM_TO_UNPACK=$unpack OSRM_AT_MOST_LONGER=$longer OSRM_AT_LEAST_DIFFERENT=$different OSRM_AT_LEAST_OPTIMAL=$optimal ./build/osrm-routed -s -a mld &
                     sleep 5
                     ./scripts/osrm-runner.js -q $REQUESTS -f "$.routes..geometry" > $OUTPUT && killall osrm-routed
@@ -30,6 +31,7 @@ for overlap in "1.25" "1.33" "1.50" "1.75"; do
     for longer in "0.25" "0.33" "0.5" "0.75"; do
         for different in "0.5" "0.75" "0.85"; do
             OUTPUT="${overlap}_${longer}_${different}_ch.csv"
+            echo "###### $OUTPUT ######"
             OSRM_OVERLAP=$overlap OSRM_AT_MOST_LONGER=$longer OSRM_AT_LEAST_DIFFERENT=$different ./build/osrm-routed -s -a ch &
             sleep 5
             ./scripts/osrm-runner.js -q $REQUESTS -f "$.routes..geometry" > $OUTPUT && killall osrm-routed
